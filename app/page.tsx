4e7f91ddd087bc523bcecf00c6f89073058fb7e3
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-03-26 16:29:05 +0100

    Added navigation between home and settings page, made design changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
         <p className={styles.text}>Eine moderne Single Page Application mit Next.js und maßgeschneidertem CSS.</p>
         <Image src="https://wallpaperaccess.com/full/4627932.jpg" alt="Coole Grafik" width={300} height={200} className={styles.image} />
         <button className={styles.button}>Drück mich!</button>
+        <Link href={"/settings"}>
+          <button className={styles.button}>Einstellungen</button>
+        </Link>
       </div>
     </div>
   );
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from "./page.module.css"
 
 export default function SettingsPage() {
     return (
         <div>
             <h1>Settings</h1>
+            <Link href={"/"}>
+                <button className={styles["button"]}>
+                    Go to home page
+                </button>
+            </Link>
         </div>
     )
 }
